0c37d7ea40c0f029553bfea856634510385af057
zombai-dev <zombai.dev@gmail.com>
2026-08-10 23:23:51 +0200

    release 0.6.2

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785860868|15650513';
+const CACHE_VERSION = '1786396927|25710420';
 /** @type {string} */
 const CACHE_PREFIX = 'Shape-Swipa-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
