8346811991e868f7d7727038da739dd9c2b48e6c
zombai-dev <zombai.dev@gmail.com>
2026-08-11 00:30:59 +0200

    update font files

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786396927|25710420';
+const CACHE_VERSION = '1786400970|699432975';
 /** @type {string} */
 const CACHE_PREFIX = 'Shape-Swipa-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
